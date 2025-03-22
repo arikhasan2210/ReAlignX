543837df538c68e768a9b5accea3e644b3acc874
Kay Leen Soh <kayleen0827@gmail.com>
2025-03-22 18:18:41 -0400

    prototype

--- voice_generator.py
+++ voice_generator.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 
-text = "You've been staring at the screen for too long. Take a short break!"
+text = "Are you falling off the screen? Let me call the ambulance!"
 tts = gTTS(text, lang='en')
-tts.save("break_reminder.mp3")
+tts.save("fallAlert.mp3")
 
 print("MP3 file saved as alertSound1.mp3")
