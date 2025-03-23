243539e6b023c55735455810bde15a3843bfc00d
Kay Leen Soh <kayleen0827@gmail.com>
2025-03-22 20:11:03 -0400

    removed files

--- voice_generator.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-
-text = "Are you falling off the screen? Let me call the ambulance!"
-tts = gTTS(text, lang='en')
-tts.save("fallAlert.mp3")
-
-print("MP3 file saved as alertSound1.mp3")
